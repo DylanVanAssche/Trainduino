55d779814d0ffc870b286a400992c60c9a637c9f
modulebaan <dylan.rscafan@gmail.com>
2013-12-29 20:03:29 +0100

    RandomLEDController Arduino Sketch V1.0 PRERELEASE
    RandomLEDController Arduino Sketch V1.0 PRERELEASE
    This is a prerelease! This version isn't completely tested. 
    A stable & fully tested release is coming soon.

---
+++ RandomLEDController/RandomLEDController.ino
@@ -0,0 +1,79 @@
+#include <SoftPWM_timer.h>
+#include <SoftPWM.h>
+#include <TimedAction.h>
+
+/*
+   ******************************************
+   *               Trainduino               *
+   *                                        *
+   *-----------> LIGHT DECODER <------------*
+   *                                        *
+   *           V 1.0.0 PRERELEASE           *
+   ******************************************
+
+   (c) Dylan Van Assche, producer of the Trainduino serie.
+*/
+
+boolean outputStatus[] = {
+  false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false
+}; // Saves the last status of the outputs.
+boolean Status = true; // Starting always in DAY mode!
+
+TimedAction DayON          =     TimedAction(random(1000, 2000), DayONFunction); // Define every timer for this sketch.
+TimedAction DayOFF         =     TimedAction(random(2500, 350), DayOFFFunction);
+TimedAction Night          =     TimedAction(random(1000, 2000), NightFunction);
+TimedAction TimerDayNight  =     TimedAction(1000, TimerDayNightFunction);
+
+int Hours = 0; // Saves the hours that are passed. 1 hour = 1 minute.
+
+void setup()
+{
+  Serial.begin(9600);
+  SoftPWMBegin();
+  for (int i = 2; i < 18; i++) // Set pinMode for output. Pin 2 -> pin 18. Pin 19 is used for the RandomSeed function.
+  {
+    SoftPWMSet(i, OUTPUT);
+    SoftPWMSetFadeTime(i, 1000, 1000);
+  }
+  randomSeed(A5);
+}
+
+void loop()
+{
+  TimerDayNight.check(); // Time tracking process.
+  if (Status) // Controls the DAY/NIGHT process.
+  {
+    DayON.check();
+    DayOFF.check();
+  }
+  else
+  {
+    Night.check();
+  }
+}
+
+void DayONFunction() // It's day, turn one output ON and another OFF.
+{
+  SoftPWMSetPercent(outputStatus[random(0, 16)], random(70, 100));
+}
+
+void DayOFFFunction()
+{
+  SoftPWMSetPercent(outputStatus[random(0, 16)], 0);
+}
+
+void NightFunction() // Turn all the outputs OFF! It's night, most of the people sleeps at this moment.
+{
+  SoftPWMSetPercent(outputStatus[random(0, 16)], 0);
+}
+
+void TimerDayNightFunction() // Time tracking function.
+{
+  Hours++;
+  Serial.print(Hours);
+  if (Hours >= 12)
+  {
+    Status = !Status;
+    Hours = 0;
+  }
+}
