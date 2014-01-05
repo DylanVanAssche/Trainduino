5ae01c2d16e469ffa5faa70d06a369876ee51237
modulebaan <dylan.rscafan@gmail.com>
2014-01-05 10:44:21 +0100

    RELEASE V1.0

--- RandomLEDController/RandomLEDController.ino
+++ RandomLEDController/RandomLEDController.ino
@@ -8,31 +8,29 @@
    *                                        *
    *--------> RANDOMLEDCONTROLLER <---------*
    *                                        *
-   *            V 0.9 PRERELEASE            *
+   *             V 1.0 RELEASE              *
    ******************************************
-   THIS IS A PRERELEASE! I'm never resposibly for any damage to your stuff! This version is also NOT completely tested.
+   THIS IS A RELEASE! I'm never resposibly for any damage to your stuff! This version is completely tested.
 
-   (c) Dylan Van Assche, producer of the Trainduino serie.
+   (c) Dylan Van Assche (2014), producer of the Trainduino serie.
 */
 
-boolean outputStatus[] = {
-  false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false
-}; // Saves the last status of the outputs.
 boolean Status = true; // Starting always in DAY mode!
 
 TimedAction DayON          =     TimedAction(random(1000, 2000), DayONFunction); // Define every timer for this sketch.
-TimedAction DayOFF         =     TimedAction(random(2500, 350), DayOFFFunction);
+TimedAction DayOFF         =     TimedAction(random(2500, 3500), DayOFFFunction);
 TimedAction Night          =     TimedAction(random(1000, 2000), NightFunction);
-TimedAction TimerDayNight  =     TimedAction(60*1000, TimerDayNightFunction);
+TimedAction Clock          =     TimedAction(1000, ClockFunction);
 
-int Hours = 0; // Saves the hours that are passed. 1 hour = 1 minute.
+int currentHour           =    7; // Start ALWAYS the program in DAY mode. Day is from 07:00 to 19:00.
+int currentMinutes        =    0;
 
 void setup()
 {
   SoftPWMBegin();
   for (int i = 2; i < 18; i++) // Set pinMode for output. Pin 2 -> pin 18. Pin 19 is used for the RandomSeed function.
   {
-    SoftPWMSet(i, OUTPUT);
+    SoftPWMSet(i, 0);
     SoftPWMSetFadeTime(i, 1000, 1000);
   }
   randomSeed(A5);
@@ -40,41 +38,50 @@ void setup()
 
 void loop()
 {
-  TimerDayNight.check(); // Time tracking process.
-  if (Status) // Controls the DAY/NIGHT process.
+  Clock.check(); // Time tracking process.
+  if(Status) // Controls the DAY/NIGHT process.
   {
     DayON.check();
     DayOFF.check();
-    SoftPWMSetPercent(17, 0);  // Deactivate the streetlights.
+    SoftPWMSetPercent(19, 0);  // Deactivate the streetlights.
   }
   else
   {
     Night.check();
-    SoftPWMSetPercent(17, 100); // Activate the streetlights.
+    SoftPWMSetPercent(19, 100); // Activate the streetlights.
   }
 }
 
 void DayONFunction() // It's day, turn one output ON and another OFF.
 {
-  SoftPWMSetPercent(outputStatus[random(0, 16)], random(70, 100));
+  SoftPWMSetPercent(random(0, 19), random(70, 100));
 }
 
 void DayOFFFunction()
 {
-  SoftPWMSetPercent(outputStatus[random(0, 16)], 0);
+  SoftPWMSetPercent(random(0, 19), 0);
 }
 
 void NightFunction() // Turn all the outputs OFF! It's night, most of the people sleeps at this moment.
 {
-  SoftPWMSetPercent(outputStatus[random(0, 16)], 0);
+  SoftPWMSetPercent(random(0, 19), 0);
 }
 
-void TimerDayNightFunction() // Time tracking function.
+void ClockFunction()
 {
-  Hours++;
-  if (Hours >= 12)
+  currentMinutes++;
+
+  if (currentMinutes == 60)
+  {
+    currentHour++;
+    currentMinutes = 0;
+
+  if (currentHour == 12)
   {
     Status = !Status;
-    Hours = 0;
+    currentHour = 0;
+  }
+    
+    
   }
 }
